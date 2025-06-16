9463d3ec8b01fe95f1ffdfb46ec7bc1e91501313
Dianna <dg109@wellesley.edu>
2025-06-16 09:49:09 -0400

    text

--- pages/Dataset.py
+++ pages/Dataset.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title('Data Collection')
-st.subheader('Methodology')
+st.header('Methodology')
 st.write('''Section 203 of the Voting Rights Act dictates that political subdivisions 
          with more than 10,000 people or over 5 percent of the population speaking a language 
          minority must provide voting materials in that language. We gathered a list of these 
@@ -14,20 +14,23 @@ st.write('''With these localities, we followed methods used in "Algorithmic Misj
          once in English and once in Spanish (232 in total) on Google. We collected the search
          result pages to note any differences in searching in each language.''')
 
-st.subheader('Raw Data')
+st.header('Raw Data')
 st.write("This is a random sample of what our raw data looked like!")
 df = pd.read_csv('engLocAccuracy1.csv')
 st.write(df.sample(n=10).style.set_properties(color='#636363'))
 
-st.subheader('Aggregation Tables')
+st.header('Aggregation Tables')
 st.write('To make the data meaningful, here are a few aggregated tables that show key differences ' \
-'in Google Search results')
+        'in Google Search results')
+st.subheader('Average Domain Position')
 count = df.groupby('domain')['domain'].value_counts()
 count = count.to_frame().reset_index().sort_values(by='domain')
 df3 = df.groupby('domain')['org-position'].mean()
-df3 = df3.to_frame().reset_index().sort_values(by='domain').rename(columns={'org-position': 'average org-position'})
+df3 = df3.to_frame().reset_index().sort_values(by='domain').rename(columns={'count':'# of appearances', 'org-position': 'average org-position'})
 df4 = pd.merge(count, df3, on='domain')
 df4 = df4.sort_values(by='count', ascending=False)
 
-st.write('Here is an aggregated table')
+st.write('Each organic result has a position on the page, with a position of 1 meaning it is the ' \
+        'very first result. In this table, the domains are sorted in descending order by number of appearances, ' \
+        'with each domain having its average organic position')
 st.dataframe(df4.style.set_properties(color='#636363'))
