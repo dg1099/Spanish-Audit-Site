f75dff3a23f51e3f861c8b1c3662d47719f38548
Dianna <dg109@wellesley.edu>
2025-06-17 10:27:02 -0400

    added spanish

--- pages/Dataset.py
+++ pages/Dataset.py
@@ -16,21 +16,27 @@ st.write('''With these localities, we followed methods used in "Algorithmic Misj
 
 st.header('Raw Data')
 st.write("This is a random sample of what our raw data looked like!")
-df = pd.read_csv('engLocAccuracy1.csv')
-st.write(df.sample(n=10).style.set_properties(color='#636363'))
+eng_df = pd.read_csv('engLocAccuracy1.csv')
+span_df = pd.read_csv('spanLocAccuracy1.csv')
+st.dataframe(eng_df.sample(n=10).style.set_properties(color='#636363'))
+st.dataframe(span_df.sample(n=10).style.set_properties(color='#636363'))
 
 st.header('Aggregation Tables')
 st.write('To make the data meaningful, here are a few aggregated tables that show key differences ' \
         'in Google Search results')
 st.subheader('Average Domain Position')
-count = df.groupby('domain')['domain'].value_counts()
-count = count.to_frame().reset_index().sort_values(by='domain')
-df3 = df.groupby('domain')['org-position'].mean()
-df3 = df3.to_frame().reset_index().sort_values(by='domain').rename(columns={'count':'# of appearances', 'org-position': 'average org-position'})
-df4 = pd.merge(count, df3, on='domain')
-df4 = df4.sort_values(by='count', ascending=False)
-
+def average_domain_pos(data):
+    df1 = eng_df.groupby('domain')['domain'].value_counts()
+    df1 = df1.to_frame().reset_index().sort_values(by='domain')
+    df2 = eng_df.groupby('domain')['org-position'].mean()
+    df2 = df2.to_frame().reset_index().sort_values(by='domain').rename(columns={'count':'# of appearances', 'org-position': 'average org-position'})
+    df3 = pd.merge(df1, df2, on='domain')
+    df3 = df3.sort_values(by='count', ascending=False)
+    return df3
+eng_adp = average_domain_pos(eng_df)
+span_adp = average_domain_pos(span_df)
 st.write('Each organic result has a position on the page, with a position of 1 meaning it is the ' \
         'very first result. In this table, the domains are sorted in descending order by number of appearances, ' \
         'with each domain having its average organic position')
-st.dataframe(df4.style.set_properties(color='#636363'))
+st.dataframe(eng_adp.style.set_properties(color='#636363'))
+st.dataframe(span_adp.style.set_properties(color='#636363'))
--- pages/Location_Analysis.py
+++ pages/Location_Analysis.py
@@ -5,20 +5,25 @@ import pandas as pd
 spanStates = ['AZ', 'CA', 'CO', 'CT', 'FL', 'GA', 'ID', 'IL', 'KS', 'MD', 'MA', 'MI', 'NE', 'NV', 'NJ', 'NM', 'NY', 'OH', 'OK', 'PA', 'RI', 'TX', 'UT', 'VA', 'WA', 'WI']
 gov_types = ['government', 'government affiliated']
 
-df = pd.read_csv('engLocAccuracy1.csv')
-df = df[df['gov_type'].isin(gov_types)]
-df = df[df['gov_state'] != 'DC']
-df = df[df['gov_state'].isin(spanStates)]
+eng_df = pd.read_csv('engLocAccuracy1.csv')
+span_df = pd.read_csv('spanLocAccuracy1.csv')
 
-df = df.groupby('state').mean('accuracy').reset_index()
-
-
-fig = px.choropleth(df, locations=df['state'].tolist(),
+def domestic_location_fig(data):
+    df = data[data['gov_type'].isin(gov_types)]
+    df = df[df['gov_state'] != 'DC']
+    df = df[df['gov_state'].isin(spanStates)]
+    df = df.groupby('state').mean('accuracy').reset_index()
+    fig = px.choropleth(df, locations=df['state'].tolist(),
                     locationmode="USA-states",
-                    color="accuracy", # lifeExp is a column of gapminder
+                    color="accuracy", 
                     range_color=[0, 1],
-                    hover_name="accuracy", # column to add to hover information
+                    hover_name="accuracy", 
                     color_continuous_scale=px.colors.make_colorscale(["red", "orange", "yellow", "lightgreen", "green"]),
                     scope='usa')
+    return fig
 
-st.plotly_chart(fig)
+eng_loc_fig = domestic_location_fig(eng_df)
+span_loc_fig = domestic_location_fig(span_df)
+
+st.plotly_chart(eng_loc_fig)
+st.plotly_chart(span_loc_fig)
