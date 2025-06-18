4ac094c2e2d87e206a70b8b030850008b8d15717
Dianna <dg109@wellesley.edu>
2025-06-18 14:29:52 -0400

    homepage

--- Homepage.py
+++ Homepage.py
@@ -7,5 +7,6 @@ st.subheader(':grey[Wellesley CredLab 2024-2025]')
 st.subheader(':grey[Project By: Dianna Gonzalez, Arli Moyao-Ramirez, Kaavya Senthil, Jenna Stephenson]')
 st.page_link(page='https://github.com/ArliMoyao/2025spring-electionsproject', label='GitHub')
 
-st.header('Introduction')
-st.write('Welcome! This our CredLab research project on the impact of the 2024 presidential elections on English and Spanish media in highly populated/dense Spanish-speaking counties in the US.')
+st.write('Welcome! This our CredLab research project on the impact of the 2024 presidential elections ' \
+        'on English and Spanish media in highly populated/dense Spanish-speaking counties in the US. ' \
+        'Navigate to the sidebar to learn more.')
